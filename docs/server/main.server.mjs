1b85038d8d61472c50aaeba664be209db6fc1e83
zead sayed <zead.sayed@oasisoft.net>
2024-12-05 14:03:22 +0200

    []

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-XTUCKHKT.mjs";import"./chunk-JT5C4GTW.mjs";import"./chunk-WJWWKD2M.mjs";import"./chunk-TRMP2QUV.mjs";import"./chunk-NFD4YNLC.mjs";import"./chunk-BLIVCY6P.mjs";import"./chunk-BNJ4LZ24.mjs";import"./chunk-HZLO2NHA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
